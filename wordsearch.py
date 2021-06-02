213748e21df006db1d5157f2686feda733b5edb5
Louka Dubois B <duboisbegl@gmail.com>
2021-06-02 02:03:21 -0400

    fixes

--- wordsearch.py
+++ wordsearch.py
@@ -28,70 +28,65 @@ def display_words_list():
 
 orientations = ['left_right', 'up_down', 'up_diagonal', 'down_diagonal']
 
-worked = False
-while worked == False:
-	try:
-		for word in words:
-			word_length = len(word)
 
-			placed = False
-			while not placed:
-				chosen_orientation = random.choice(orientations)
+for word in words:
+	word_length = len(word)
 
-				if chosen_orientation == 'left_right':
-					step_x = 1
-					step_y = 0
-				if chosen_orientation == 'up_down':
-					step_x = 0
-					step_y = 1
-				if chosen_orientation == 'up_diagonal':
-					step_x = 1
-					step_y = -1
-				if chosen_orientation == 'down_diagonal':
-					step_x = 1
-					step_y = 1
+	placed = False
+	while not placed:
+		chosen_orientation = random.choice(orientations)
 
-				position_x = random.randint(0, grid_size)
-				position_y = random.randint(0, grid_size)
+		if chosen_orientation == 'left_right':
+			step_x = 1
+			step_y = 0
+		if chosen_orientation == 'up_down':
+			step_x = 0
+			step_y = 1
+		if chosen_orientation == 'up_diagonal':
+			step_x = 1
+			step_y = -1
+		if chosen_orientation == 'down_diagonal':
+			step_x = 1
+			step_y = 1
 
-				ending_x = position_x + word_length * step_x
-				ending_y = position_y + word_length * step_y
+		position_x = random.randint(0, grid_size)
+		position_y = random.randint(0, grid_size)
 
-				if ending_x < 0 or ending_x >= grid_size: continue
-				if ending_y < 0 or ending_y >= grid_size: continue
+		ending_x = position_x + word_length * step_x
+		ending_y = position_y + word_length * step_y
 
-				failed = False
+		if ending_x < 0 or ending_x >= grid_size: continue
+		if ending_y < 0 or ending_y >= grid_size: continue
 
-				for i in range(word_length):
-					letter = word[i]
-					new_position_x = position_x + i * step_x
-					new_position_y = position_y + i * step_y
+		failed = False
 
-					letter_at_new_position = grid[new_position_x][new_position_y]
-					if letter_at_new_position != '•':
-						if letter_at_new_position == letter:
-							continue
-						else:
-							failed = True
-							break
+		for i in range(word_length):
+			letter = word[i]
+			new_position_x = position_x + i * step_x
+			new_position_y = position_y + i * step_y
 
-				if failed:
+			letter_at_new_position = grid[new_position_x][new_position_y]
+			if letter_at_new_position != '•':
+				if letter_at_new_position == letter:
 					continue
 				else:
-					for i in range(word_length):
-						letter = '\33[31m'+word[i]+'\33[0m'
-						new_position_x = position_x + i * step_x
-						new_position_y = position_y + i * step_y
-						grid[new_position_x][new_position_y] = letter
-					placed = True
+					failed = True
+					break
 
-		for a in range(grid_size):
-			for b in range(grid_size):
-				if grid[a][b] == '•':
-					grid[a][b] = random.choice(string.ascii_uppercase)
-		worked = True
-	except:
-		pass
+		if failed:
+			continue
+		else:
+			for i in range(word_length):
+				letter = '\33[31m'+word[i]+'\33[0m'
+				new_position_x = position_x + i * step_x
+				new_position_y = position_y + i * step_y
+				grid[new_position_x][new_position_y] = letter
+			placed = True
+
+for a in range(grid_size):
+	for b in range(grid_size):
+		if grid[a][b] == '•':
+			grid[a][b] = random.choice(string.ascii_uppercase)
 
 print_grid()
 display_words_list()
